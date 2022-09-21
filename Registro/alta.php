0a3a42ff9764b9186b525f88d76dcea247bd8bf8
Aitor Menacho <menacho.aitor@gmail.com>
2022-09-21 14:03:33 +0200

    Update alta.php

--- Registro/alta.php
+++ Registro/alta.php
@@ -1,4 +1,17 @@
 <?php
 
+//Nos conectamos con la base de datos.
+include('db.php');
+
+//Insertamos datos.
+$query = "INSERT INTO alta(Usuario_nombre, Usuario_apellido1, Usuario_apellido2, Usuario_clave, Usuario_fecha_alta, Usuario_email) values 
+                        ('$_REQUEST[nombre]',
+                         '$_REQUEST[apellido1]',
+                         '$_REQUEST[apellido2]',
+                         '$_REQUEST[clave]',
+                         '$fecha_alta',
+                         '$_REQUEST[email]')";
+
+        $registro = mysqli_query($conexion, $query) or die("Problemas al registrar usuario: " . mysqli_error($conexion));
 
 ?>
