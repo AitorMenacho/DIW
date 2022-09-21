5b065fd4616df146463d0aec2141da4c8ffc937b
Aitor Menacho <menacho.aitor@gmail.com>
2022-09-21 13:58:25 +0200

    Add connection

---
+++ Registro/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombreserver = "localhost";
+$basededatos = "instituto";
+$nombreusuario = "root";
+$contrasenna = "";
+
+$conexion=mysqli_connect($nombreserver,$nombreusuario,$contrasenna,$basededatos);
+
+
+?>
